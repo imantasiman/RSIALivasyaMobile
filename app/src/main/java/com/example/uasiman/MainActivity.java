998545670ed13439f015fa6c1e7f29ef2020d463
macbook <imantasiman07@gmail.com>
2023-07-23 14:52:01 +0700

    Menambahkan Calendar EditText di MainActivity

--- app/src/main/java/com/example/uasiman/MainActivity.java
+++ app/src/main/java/com/example/uasiman/MainActivity.java
@@ -2,19 +2,26 @@ package com.example.uasiman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
     private EditText editTextName, editTextAddress, editTextPhoneNumber, editTextDate;
+    private Calendar calendar;
     private Button buttonSave;
     private ListView listViewClinics;
     private Button btnKembali;
@@ -30,9 +37,37 @@ public class MainActivity extends AppCompatActivity {
         editTextAddress = findViewById(R.id.editTextAddress);
         editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
         editTextDate = findViewById(R.id.editTextDate);
+        editTextDate=findViewById(R.id.editTextDate);
         buttonSave = findViewById(R.id.buttonSave);
         btnKembali = findViewById(R.id.btnKembali);
         listViewClinics = findViewById(R.id.listViewClinics);
+
+        Calendar calendar=Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener date=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                updateCalendar();
+            }
+            private void updateCalendar(){
+                String Format="MM/dd/yy";
+                SimpleDateFormat sdf= new SimpleDateFormat(Format, Locale.US);
+                editTextDate.setText(sdf.format(calendar.getTime()));
+            }
+        };
+        editTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(MainActivity.this, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+
+        });
+{
+
+        }
 
         clinicList = new ArrayList<>();
         clinicAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, clinicList);
@@ -62,6 +97,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+
         btnKembali.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
