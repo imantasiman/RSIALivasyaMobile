a38d789f2d84adcd3ce5156729c7e4eade302e2c
macbook <imantasiman07@gmail.com>
2023-07-23 17:14:24 +0700

    Megubah margin Splashscreen  di MainActivity

--- app/src/main/java/com/example/uasiman/LoginActivity.java
+++ app/src/main/java/com/example/uasiman/LoginActivity.java
@@ -6,11 +6,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText editTextUsername, editTextPassword;
     private Button buttonLogin;
+    private TextView noLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextPassword = findViewById(R.id.editTextPassword);
         buttonLogin = findViewById(R.id.buttonLogin);
+        noLogin = findViewById(R.id.noLogin);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +32,18 @@ public class LoginActivity extends AppCompatActivity {
 
                 // Ganti dengan validasi login sesuai kebutuhan
                 Intent intent = new Intent(LoginActivity.this, BerandaActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+
+        });
+        noLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Ganti dengan validasi login sesuai kebutuhan
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
 
